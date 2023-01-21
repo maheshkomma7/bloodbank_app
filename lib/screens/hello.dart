b7f6c2a9d9d308dc48fea32f97d4d9f3a7865f5a
maheshkomma7 <kommaumamaheswarreddy@gmail.com>
2023-01-21 16:30:33 +0530

    updated

--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -7,8 +7,10 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-        child: Image.asset("assets/bloodbank.png"),),
+        child: Center(
+          child: Container(
+          child: Image.asset("assets/bloodbank.png"),),
+        ),
       ),
     );
   }
