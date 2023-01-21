673f2b66e9d5a58cc3bf24fe549cf7c21ec96a93
maheshkomma7 <kommaumamaheswarreddy@gmail.com>
2023-01-21 13:49:39 +0530

    onborading_screen

---
+++ lib/constants/onboardingdata.dart
@@ -0,0 +1,12 @@
+List<Map<String, String>> onboardingdata = [{
+
+  "image_url":"assets/health-check 1.png",
+  "text":"To be a responsible donor, you must get a check-up."
+}
+,{
+ "image_url":"assets/puzzle 1.png",
+  "text":"Your blood type should be compatible with the receiver’s type."
+},{
+ "image_url":"assets/blood-bag 1.png",
+  "text":"Donate your blood and save a life."
+}];
